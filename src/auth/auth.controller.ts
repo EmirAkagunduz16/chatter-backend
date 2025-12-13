542a01da10c5fb2f1a49bcd58efc81f9d3214577
EmirAkagunduz16 <akagunduzemir04@gmail.com>
2025-12-13 22:40:11 +0300

    upd

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Post, Res } from '@nestjs/common';
+import { Controller, Post, Res, UseGuards } from '@nestjs/common';
 import { CurrentUser } from './current-user.decorator';
 import { User } from 'src/users/entities/user.entity';
 import type { Response } from 'express';
 import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(
     @CurrentUser() user: User,
