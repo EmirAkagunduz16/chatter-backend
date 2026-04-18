29d7392c97f5c56c3f2a8d0955ee7f1f6b21b743
EmirAkagunduz16 <akagunduzemir04@gmail.com>
2026-04-18 14:55:42 +0300

    upd

--- src/app.module.ts
+++ src/app.module.ts
@@ -26,7 +26,6 @@ import { AuthService } from './auth/auth.service';
       driver: ApolloDriver,
       useFactory: (authService: AuthService) => ({
         autoSchemaFile: true,
-        cors: true,
         subscriptions: {
           'graphql-ws': {
             onConnect: (context: any) => {
