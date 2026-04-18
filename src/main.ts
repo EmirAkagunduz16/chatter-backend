f232d1fb57f116e7654431c1138532561da20deb
EmirAkagunduz16 <akagunduzemir04@gmail.com>
2026-04-18 13:56:05 +0300

    upd

--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.useLogger(app.get(Logger));
   app.use(cookieParser.default());
-  app.enableCors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-  });
+  app.enableCors();
 
   const configService = app.get(ConfigService);
   await app.listen(configService.getOrThrow('PORT'));
